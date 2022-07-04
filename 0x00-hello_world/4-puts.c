eb7468c17157bd345c5a843517e68e3533a24267
Liratau <liraratau@gmail.comclear>
2022-07-04 06:31:02 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include <studio.h>
+#include <stdio.h>
 
-int main ()
-
+int main(void)
 {
 
-	puts ("\" Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-	return 0;
+	return (0);
 
 }
