6556dc17925990f1655d60819fe3151013800e56
Liratau <liraratau@gmail.comclear>
2022-07-04 20:16:11 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-/* Description: 'the program's description' */
-int main(void) /* what */
+/**
+*main- prints the size of various types
+*Return:0 if exited properly, non zero otherwise
+*/
+int main(void)
 {
-	puts(i"Programming is like building a multilingual puzzle");
-	return (0);
+puts("Programming is like building a multilingual puzzle");
+return (0);
 }
