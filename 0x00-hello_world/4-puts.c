8afad31a91ca54f835d717f7eca8a75d0902150c
Liratau <liraratau@gmail.comclear>
2022-07-04 19:36:10 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /* Description: 'the program's description' */
-int main(void)
+int main(void) /* what */
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts(i"Programming is like building a multilingual puzzle");
 	return (0);
 }
